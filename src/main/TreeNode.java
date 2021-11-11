b851dc80ca4a7bdc667f954040571fabc5ea939e
Braydon Pacheco <braydonpacheco@gmail.com>
2021-11-11 13:56:34 -0700

    Added control classes for fleaf and ftrans, with basic descriptions of functions. Also added the TreeNode class to represent our search tree, and the ProblemState class to represent the state of each TreeNode

---
+++ src/main/TreeNode.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.ArrayList;
+
+import main.problem.ProblemState;
+
+public class TreeNode {
+
+	TreeNode parent;
+	ArrayList<TreeNode> children;
+	ProblemState state;
+	
+	public TreeNode(ProblemState state, TreeNode parent) {
+		this.parent = parent;
+		this.state = state;
+	}
+	
+	public TreeNode getParent() {
+		return parent;
+	}
+	public ArrayList<TreeNode> getChildren() {
+		return children;
+	}
+	public ProblemState getState() {
+		return state;
+	}
+	
+	public void setChildren(ArrayList<TreeNode> children) {
+		this.children = children;
+	}
+	
+	public void addChild(TreeNode child) {
+		children.add(child);
+	}
+
+	public boolean isLeaf() {
+		if (children.isEmpty()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean isRoot() {
+		if (parent == null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
---
+++ src/main/problem/ProblemState.java
@@ -0,0 +1,38 @@
+package main.problem;
+
+public class ProblemState {
+
+	private Problem problem;
+	private Boolean sol = null;
+	private int eval;
+	
+	public ProblemState(Problem problem) {
+		this.problem = problem;
+	}
+
+	public Problem getProblem() {
+		return problem;
+	}
+
+	public void setProblem(Problem problem) {
+		this.problem = problem;
+	}
+
+	public Boolean getSol() {
+		return sol;
+	}
+
+	public void setSol(Boolean sol) {
+		this.sol = sol;
+	}
+
+	public int getEval() {
+		return eval;
+	}
+
+	public void setEval(int eval) {
+		this.eval = eval;
+	}
+	
+	
+}

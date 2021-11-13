cd9531abc6c5a9c34d8a91e9d6ff29f5c1b33014
Peter Lefaivre <noodlegamesdev@gmail.com>
2021-11-13 15:53:17 -0700

    Parser: Implemented parseCourses and parseLabs

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -13,15 +13,20 @@ public class Parser {
 	//IDEA: have functions to return each type of data present in the input file. ie. slots, course identifiers, pairs, etc.
 	//Each return should be of a data type that coincides with the object being represented.
 	
-	//Define global variables
+	//
+	//GLOBAL VARIABLES
+	//
 	private File inputFile;
+	private String name;
+	private ArrayList<CourseSlot> courseSlots;
+	private ArrayList<LabSlot> labSlots;
+	private ArrayList<Course> courses;
+	private ArrayList<Lab> labs;
 	
-	//Parser - Constructor for a new Parser
-	//INPUT: input: The file to parse
-	//RETURNS: None
-	public Parser(String input) throws Exception {
-		inputFile = new File(input);
-	}
+	
+	//
+	//UTILITY METHODS
+	//
 	
 	//createScanner - Creates a new scanner to parse an input file
 	//INPUT: None
@@ -32,6 +37,9 @@ public class Parser {
 		return scanner;
 	}
 	
+	//getDay - Returns the enum type of a day, given a matching string type
+	//INPUT: String representing abbreviated day
+	//RETURNS: A Day enum type
 	private Day getDay(String string) throws Exception{
 		if (string.equals("MO")) {
 			return Day.MO;
@@ -45,10 +53,27 @@ public class Parser {
 		throw new Exception("Invalid day in input file!");
 	}
 	
+	//getCourse - Returns a course object contained in the courses arraylist with the same name as the input string
+	//INPUT: Course name string of form "<department> <number> <section>"
+	//RETURNS: A course object if one is found
+	private Course getCourse(String string) throws Exception {
+		for (Course course : courses) {
+			if(course.getName().equals(string)){
+				return course;
+			}
+		}
+		throw new Exception("Invalid course in input file!");
+	}
+	
+	
+	//
+	//PARSE METHODS
+	//
+	
 	//parseName - Parses input file for the name of the example
 	//INPUT: None
 	//RETURNS: A string representing the name of the example
-	public String parseName() throws Exception {
+	private String parseName() throws Exception {
 		Scanner scanner = createScanner();
 		String name = "";
 		while (scanner.hasNextLine()) {
@@ -67,7 +92,7 @@ public class Parser {
 	//parseCourseSlots - Parses input file for course slots
 	//INPUT: None
 	//RETURNS: An arraylist of all course slots
-	public ArrayList<CourseSlot> parseCourseSlots() throws Exception {
+	private ArrayList<CourseSlot> parseCourseSlots() throws Exception {
 		Scanner scanner = createScanner();
 		String slotString;
 		ArrayList<CourseSlot> slots = new ArrayList<CourseSlot>();
@@ -87,22 +112,101 @@ public class Parser {
 				}
 			}
 		}
+		scanner.close();
 		return slots;
 	}
 	
-	public ArrayList<LabSlot> parseLabSlots() {
-		return null;
+	//parseLabSlots - Parses input file for lab slots
+	//INPUT: None
+	//RETURNS: An arraylist of all lab slots
+	private ArrayList<LabSlot> parseLabSlots() throws Exception {
+		Scanner scanner = createScanner();
+		String slotString;
+		ArrayList<LabSlot> slots = new ArrayList<LabSlot>();
+		while(scanner.hasNextLine()) {
+			
+			if(scanner.nextLine().equals("Lab slots:")) {
+				
+				slotString = scanner.nextLine();
+				while(slotString.equals("") == false) {
+					slotString = slotString.replaceAll("\\s","");
+					String[] parts = slotString.split(",");
+					if(parts.length != 4) {
+						throw new Exception("Invalid lab slot in input file!");
+					}
+					slots.add(new LabSlot(getDay(parts[0]), LocalTime.parse(parts[1], DateTimeFormatter.ofPattern("H:m")), Integer.parseInt(parts[2]), Integer.parseInt(parts[3])));
+					slotString = scanner.nextLine();
+				}
+			}
+		}
+		scanner.close();
+		return slots;
 	}
 	
-	public ArrayList<Course> parseCourses() {
-		return null;
+	//parseCourses - Parses input file for courses
+	//INPUT: None
+	//RETURNS: An arraylist of all courses
+	private ArrayList<Course> parseCourses() throws Exception {
+		Scanner scanner = createScanner();
+		String slotString;
+		ArrayList<Course> slots = new ArrayList<Course>();
+		while(scanner.hasNextLine()) {
+			
+			if(scanner.nextLine().equals("Courses:")) {
+				
+				slotString = scanner.nextLine();
+				while(slotString.equals("") == false) {
+					slotString = slotString.trim().replaceAll(" +", " ");
+					String[] parts = slotString.split(" ");
+					if(parts.length != 4) {
+						throw new Exception("Invalid course in input file!");
+					}
+					slots.add(new Course(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[3])));
+					slotString = scanner.nextLine();
+				}
+			}
+		}
+		scanner.close();
+		return slots;
 	}
 	
-	public ArrayList<Lab> parseLabs() {
-		return null;
+	//parseCourses - Parses input file for labs
+	//INPUT: None
+	//RETURNS: An arraylist of all labs
+	private ArrayList<Lab> parseLabs() throws Exception {
+		Scanner scanner = createScanner();
+		String slotString;
+		ArrayList<Lab> slots = new ArrayList<Lab>();
+		while(scanner.hasNextLine()) {
+			
+			if(scanner.nextLine().equals("Labs:")) {
+				
+				slotString = scanner.nextLine();
+				while(slotString.equals("") == false) {
+					slotString = slotString.trim().replaceAll(" +", " ");
+					String[] parts = slotString.split(" ");
+					//Lab specific to a lecture section
+					if(parts.length == 6) {
+						Course course = getCourse(parts[0] + " " + parts[1] + " " + Integer.parseInt(parts[3]));
+						slots.add(new Lab(course, Integer.parseInt(parts[5]), false));
+					}
+					//Lab open to all lecture sections
+					else if (parts.length == 4){
+						Course course = getCourse(parts[0] + " " + parts[1] + " " + 1);
+						slots.add(new Lab(course, Integer.parseInt(parts[3]), true));
+					}
+					else {
+						throw new Exception("Invalid lab in input file!");
+					}
+					slotString = scanner.nextLine();
+				}
+			}
+		}
+		scanner.close();
+		return slots;
 	}
 	
-	public ArrayList<Element[]> parseNotCompatible(){
+	private ArrayList<Element[]> parseNotCompatible(){
 		return null;
 	}
 	
@@ -119,7 +223,7 @@ public class Parser {
 	*/
 	
 	
-	public ArrayList<Element[]> parsePair(){
+	private ArrayList<Element[]> parsePair(){
 		return null;
 	}
 	
@@ -129,4 +233,40 @@ public class Parser {
 	}
 	*/
 	
+	//parseFile - Main parse method. Invokes sub-parse methods to populate global vars with data from input file
+	//INPUT: A string representing the local path of a .txt file that should contain valid input for the system
+	//RETURNS: None
+	public void parseFile(String path) throws Exception {
+		inputFile = new File(path);
+    	name = parseName();
+    	courseSlots = parseCourseSlots();
+    	labSlots = parseLabSlots();
+    	courses = parseCourses();
+    	labs = parseLabs();
+	}
+	
+	
+	//
+	//GETTERS
+	//
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<CourseSlot> getCourseSlots(){
+		return courseSlots;
+	}
+	
+	public ArrayList<LabSlot> getlabSlots(){
+		return labSlots;
+	}
+	
+	public ArrayList<Course> getCourses(){
+		return courses;
+	}
+	
+	public ArrayList<Lab> getLabs(){
+		return labs;
+	}
 }
--- src/main/Start.java
+++ src/main/Start.java
@@ -11,11 +11,9 @@ class Start {
     	if (args.length == 0) {
     		throw new FileNotFoundException("Please provide the path of a valid input file.");
     	}
-    	//Instantiate Parser
-    	Parser parser = new Parser(args[0]);
+    	//Instantiate Parser, parse file passed in arg[0]
+    	Parser parser = new Parser();
+    	parser.parseFile(args[0]);
     	
-    	//Parse and store all information from input file
-    	String name = parser.parseName();
-    	ArrayList<CourseSlot> courseSlots = parser.parseCourseSlots();
     }
 }

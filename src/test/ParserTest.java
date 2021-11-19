14b64e0a2a7973dcc6837d8199188681554e59a6
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-11-19 12:34:54 -0700

    Updated parser test

--- src/test/ParserTest.java
+++ src/test/ParserTest.java
@@ -23,7 +23,26 @@ public class ParserTest {
     }
     
     @Test
-    public void testParser() {
+    public void testParseName() {
         assertEquals("ShortExample", parser.getName());
+    }
+
+    @Test
+    public void testParseCourses() {
+        assertEquals(4, parser.getCourses().size());
+        assertEquals("CPSC", parser.getCourses().get(0).getDepartment());
+        assertEquals(433, parser.getCourses().get(0).getNumber());
+        assertEquals(1, parser.getCourses().get(0).getSection());
+        assertEquals("CPSC", parser.getCourses().get(1).getDepartment());
+        assertEquals(433, parser.getCourses().get(1).getNumber());
+        assertEquals(2, parser.getCourses().get(1).getSection());        
+    }
+
+
+    @Test
+    public void testParseLabs() {
+        assertEquals(4, parser.getLabs().size());        
+        assertEquals(parser.getCourses().get(0), parser.getLabs().get(0).getCourse());
+        assertEquals(1, parser.getLabs().get(0).getNumber());
     }
 }

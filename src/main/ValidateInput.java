e09e01f02f7061b263129f249ca1d7b59b37bbec
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-11-25 08:51:42 -0700

    Tested Couse Validation

--- src/main/ValidateInput.java
+++ src/main/ValidateInput.java
@@ -11,23 +11,23 @@ public class ValidateInput {
 	
 	private static DateTimeFormatter format = DateTimeFormatter.ofPattern("H:m");
 
-    private static LocalTime[] course_start_times_monday = { LocalTime.parse("8:00", format), LocalTime.parse("9:00", format),
+    private static LocalTime[] course_start_times_monday = { LocalTime.parse("08:00", format), LocalTime.parse("09:00", format),
             LocalTime.parse("10:00", format), LocalTime.parse("11:00", format), LocalTime.parse("12:00", format), LocalTime.parse("13:00", format),
-            LocalTime.parse("14:00", format), LocalTime.parse("15:00", format), LocalTime.parse("16:00", format), LocalTime.parse("18:00", format),
+            LocalTime.parse("14:00", format), LocalTime.parse("15:00", format), LocalTime.parse("16:00", format), LocalTime.parse("17:00", format),LocalTime.parse("18:00", format),
             LocalTime.parse("19:00", format), LocalTime.parse("20:00", format), };
 
-    private static LocalTime[] course_start_times_tuesday = { LocalTime.parse("8:00", format), LocalTime.parse("9:30", format),
+    private static LocalTime[] course_start_times_tuesday = { LocalTime.parse("08:00", format), LocalTime.parse("09:30", format),
             LocalTime.parse("11:00", format), LocalTime.parse("12:30", format), LocalTime.parse("14:00", format), LocalTime.parse("15:30", format),
             LocalTime.parse("17:00", format), LocalTime.parse("18:30", format), };
 
-    private static LocalTime[] lab_start_times_monday = { LocalTime.parse("8:00", format), LocalTime.parse("9:00", format),
+    private static LocalTime[] lab_start_times_monday = { LocalTime.parse("08:00", format), LocalTime.parse("09:00", format),
             LocalTime.parse("10:00", format), LocalTime.parse("11:00", format), LocalTime.parse("12:00", format), LocalTime.parse("13:00", format),
             LocalTime.parse("14:00", format), LocalTime.parse("15:00", format), LocalTime.parse("16:00", format), LocalTime.parse("17:00", format),
             LocalTime.parse("18:00", format), LocalTime.parse("19:00", format), LocalTime.parse("20:00", format), };
 
     private static LocalTime[] lab_start_times_tuesday = lab_start_times_monday;
 
-    private static LocalTime[] lab_start_times_friday = { LocalTime.parse("8:00", format), LocalTime.parse("10:00", format),
+    private static LocalTime[] lab_start_times_friday = { LocalTime.parse("08:00", format), LocalTime.parse("10:00", format),
             LocalTime.parse("12:00", format), LocalTime.parse("14:00", format), LocalTime.parse("16:00", format), LocalTime.parse("18:00", format), };
 
     // courseSlot - Checks the validity of the Course Slot slot

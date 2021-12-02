1ac723e0c5bc0beb2305bac779001a01b4107e47
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-12-02 11:39:18 -0700

    Added correct slot type check for CPSC 813 and CPSC 913

--- src/main/Constr.java
+++ src/main/Constr.java
@@ -13,30 +13,31 @@ public class Constr {
     private Hashtable<Slot, Integer> labs_in_slot;
 
     private final boolean DEBUG = false;
-    
+
     public Constr() {
         courses_in_slot = new Hashtable<Slot, Integer>();
         labs_in_slot = new Hashtable<Slot, Integer>();
     }
-    
-    //Copy constructor
+
+    // Copy constructor
     public Constr(Constr toCopy) {
         courses_in_slot = new Hashtable<Slot, Integer>();
         labs_in_slot = new Hashtable<Slot, Integer>();
 
         Set<Slot> courses_in_slots_keys = toCopy.get_course_slot_list().keySet();
-        for(Slot slot : courses_in_slots_keys) {
+        for (Slot slot : courses_in_slots_keys) {
             this.courses_in_slot.put(slot, toCopy.get_course_slot_list().get(slot));
         }
-        
+
         Set<Slot> labs_in_slots_keys = toCopy.get_lab_slot_list().keySet();
-        for(Slot slot : labs_in_slots_keys) {
+        for (Slot slot : labs_in_slots_keys) {
             this.labs_in_slot.put(slot, toCopy.get_lab_slot_list().get(slot));
         }
     }
 
     public boolean checkConstraints(ArrayList<Assignment> assignments) {
-    	if (assignments == null || assignments.size() == 0) return true;
+        if (assignments == null || assignments.size() == 0)
+            return true;
         int last_index = assignments.size() - 1;
 
         Assignment new_addition = assignments.get(last_index);
@@ -46,17 +47,20 @@ public class Constr {
         for (Assignment assignment : assignments.subList(0, last_index)) {
 
             if (conflictingCourseWithLab(new_addition, assignment)) {
-            	if (DEBUG) System.out.println("conflict course with lab");
+                if (DEBUG)
+                    System.out.println("conflict course with lab");
                 return false;
             }
 
             if (failsCompatability(new_addition, assignment)) {
-            	if (DEBUG) System.out.println("conflict compatability");
+                if (DEBUG)
+                    System.out.println("conflict compatability");
                 return false;
             }
 
             if (collisionOn500LevelCourses(new_addition, assignment)) {
-            	if (DEBUG) System.out.println("conflict 500 level course");
+                if (DEBUG)
+                    System.out.println("conflict 500 level course");
                 return false;
             }
 
@@ -68,27 +72,33 @@ public class Constr {
     private boolean assignmentFails(Assignment assignment) {
 
         if (failsCorrectSlotType(assignment)) {
-        	if (DEBUG) System.out.println("failed correct slot type");
+            if (DEBUG)
+                System.out.println("failed correct slot type");
             return true;
         }
         if (failsMaxElements(assignment)) {
-        	if (DEBUG) System.out.println("failed max elements");
+            if (DEBUG)
+                System.out.println("failed max elements");
             return true;
         }
         if (failsUnwanted(assignment)) {
-        	if (DEBUG) System.out.println("failed unwanted");
+            if (DEBUG)
+                System.out.println("failed unwanted");
             return true;
         }
         if (failsEveningSlot(assignment)) {
-        	if (DEBUG) System.out.println("failed evening slot");
+            if (DEBUG)
+                System.out.println("failed evening slot");
             return true;
         }
         if (failsTuesdayCourseRestriction(assignment)) {
-        	if (DEBUG) System.out.println("failed tuesday course restriction");
+            if (DEBUG)
+                System.out.println("failed tuesday course restriction");
             return true;
         }
         if (failsPlacementOf813And913(assignment)) {
-        	if (DEBUG) System.out.println("failed 813/913");
+            if (DEBUG)
+                System.out.println("failed 813/913");
             return true;
         }
         return false;
@@ -97,6 +107,13 @@ public class Constr {
     private boolean failsCorrectSlotType(Assignment assignment) {
         Slot slot = assignment.getSlot();
         Element element = assignment.getElement();
+
+        if (element instanceof Course) {
+            Course course = (Course) element;            
+            if (course.getDepartment() == "CPSC" &&
+                ( course.getNumber() == 913 || course.getNumber() == 813 ))
+                return slot instanceof CourseSlot;
+        }
 
         if (slot instanceof LabSlot)
             return !(element instanceof Lab);
@@ -122,9 +139,8 @@ public class Constr {
     private void incrementElementCount(Hashtable<Slot, Integer> elements_in_slot, Slot slot) {
         if (elements_in_slot.containsKey(slot)) {
             elements_in_slot.put(slot, elements_in_slot.get(slot) + 1);
-        }
-        else {
-        	elements_in_slot.put(slot, 1);
+        } else {
+            elements_in_slot.put(slot, 1);
         }
     }
 
@@ -132,7 +148,8 @@ public class Constr {
 
         ArrayList<Assignment> unwanted_assignments = Parser.getUnwanted();
         for (Assignment unwanted : unwanted_assignments) {
-            if (unwanted.equals(assignment)) return true;
+            if (unwanted.equals(assignment))
+                return true;
         }
         return false;
     }
@@ -165,17 +182,20 @@ public class Constr {
     }
 
     private boolean failsTuesdayCourseRestriction(Assignment a) {
-    	if (a.getElement() instanceof Lab) return false;
-    	
-    	Course course = (Course) a.getElement();  	
-    	if (!course.getDepartment().equals("CPSC")) return false;
-    	
+        if (a.getElement() instanceof Lab)
+            return false;
+
+        Course course = (Course) a.getElement();
+        if (!course.getDepartment().equals("CPSC"))
+            return false;
+
         Slot slot = a.getSlot();
-        if (slot.getDay() != Day.TU) return false;
+        if (slot.getDay() != Day.TU)
+            return false;
 
         LocalTime eleven = LocalTime.of(11, 00);
         return slot.getStartTime().compareTo(eleven) == 0;
-        
+
     }
 
     private boolean failsPlacementOf813And913(Assignment assignment) {
@@ -230,8 +250,8 @@ public class Constr {
         Day dayOne = one.getDay();
         Day dayTwo = two.getDay();
 
-        if(dayOne == dayTwo || (dayOne == Day.MO && dayTwo == Day.FR)
-                || (dayTwo == Day.MO && dayOne == Day.FR)){
+        if (dayOne == dayTwo || (dayOne == Day.MO && dayTwo == Day.FR)
+                || (dayTwo == Day.MO && dayOne == Day.FR)) {
             LocalTime startOne = one.getStartTime();
             LocalTime startTwo = two.getStartTime();
             if (startOne.equals(startTwo)) {
@@ -257,8 +277,10 @@ public class Constr {
         ArrayList<Element[]> notCompatible = Parser.getNotCompatible();
 
         for (Element[] elem : notCompatible) {
-            if ((firstElement.equals(elem[0]) && secondElement.equals(elem[1]) && slotsOverlap(first.getSlot(),second.getSlot()))
-                    || (secondElement.equals(elem[0]) && firstElement.equals(elem[1]) && slotsOverlap(first.getSlot(), second.getSlot()))) {
+            if ((firstElement.equals(elem[0]) && secondElement.equals(elem[1])
+                    && slotsOverlap(first.getSlot(), second.getSlot()))
+                    || (secondElement.equals(elem[0]) && firstElement.equals(elem[1])
+                            && slotsOverlap(first.getSlot(), second.getSlot()))) {
                 return true;
             }
         }
@@ -286,13 +308,13 @@ public class Constr {
 
         return slotsOverlap(firstSlot, secondSlot);
     }
-    
+
     public Hashtable<Slot, Integer> get_course_slot_list() {
-    	return courses_in_slot;
+        return courses_in_slot;
     }
-    
+
     public Hashtable<Slot, Integer> get_lab_slot_list() {
-    	return labs_in_slot;
+        return labs_in_slot;
     }
-    
+
 }

0fe76aece766922ffffe3b1605f235fdf9f6e096
Juan Pablo Lozano Sarmiento <jp@osloz.com>
2021-12-06 11:28:19 -0700

    Revert "Added pen vals"
    This reverts commit bef42e675b66f337da18b85f342e25bbff43acb3.

--- src/main/Env.java
+++ src/main/Env.java
@@ -2,24 +2,19 @@ package main;
 
 //Static class which keeps track of 
 public class Env {
-	private static float minPenalty = Float.MAX_VALUE;
-	private static float minfilledWeight;
-	private static float prefWeight;
-	private static float pairWeight;
-	private static float secdiffWeight;
+    private static float minPenalty = Float.MAX_VALUE;
+    private static float minfilledWeight;
+    private static float prefWeight;
+    private static float pairWeight;
+    private static float secdiffWeight;
+    
+    public static float getMinPenalty() {
+        return minPenalty;
+    }
 
-	private static float pen_coursemin;
-	private static float pen_labsmin;
-	private static float pen_notpaired;
-	private static float pen_section;
-
-	public static float getMinPenalty() {
-		return minPenalty;
-	}
-
-	public static void setMinPenalty(float minPen) {
-		minPenalty = minPen;
-	}
+    public static void setMinPenalty(float minPen) {
+        minPenalty = minPen;
+    }
 
 	public static float getMinfilledWeight() {
 		return minfilledWeight;
@@ -52,37 +47,6 @@ public class Env {
 	public static void setSecdiffWeight(float secdiffWeight) {
 		Env.secdiffWeight = secdiffWeight;
 	}
-
-	public static float getPenCoursemin() {
-		return pen_coursemin;
-	}
-
-	public static float getPenLabsmin() {
-		return pen_labsmin;
-	}
-
-	public static float getPenNotpaired() {
-		return pen_notpaired;
-	}
-
-	public static float getPenSection() {
-		return pen_section;
-	}
-
-	public static void setPenCoursemin(float new_value) {
-		pen_coursemin = new_value;
-	}
-
-	public static void setPenlabsmin(float new_value) {
-		pen_labsmin = new_value;
-	}
-
-	public static void setPenNotpaired(float new_value) {
-		pen_notpaired = new_value;
-	}
-
-	public static void setPenSection(float new_value) {
-		pen_section = new_value;
-	}
-
+    
+    
 }
